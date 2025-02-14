7dbf7ebd91dddd19749ad578b061e8c847477d1d
jingeon <ephigh1397@naver.com>
2025-02-14 22:51:18 +0900

    feature: UsersRepository 추가
    ID 중복확인을 위한 existsByLoginId 추가

---
+++ src/main/java/jg/aurora_world/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package jg.aurora_world.repository;
+
+import jg.aurora_world.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Boolean existsByLoginId(String loginId);
+}
