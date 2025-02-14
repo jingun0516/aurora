7fffa3a6255d00c225d84ef1986bcafdab0cdc2c
jingeon <ephigh1397@naver.com>
2025-02-14 17:24:46 +0900

    feature: Users Entity 생성

---
+++ src/main/java/jg/aurora_world/entity/Users.java
@@ -0,0 +1,27 @@
+package jg.aurora_world.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id", nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Users(String loginId, String password) {
+        this.loginId = loginId;
+        this.password = password;
+    }
+}
