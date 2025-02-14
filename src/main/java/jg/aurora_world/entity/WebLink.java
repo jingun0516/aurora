6038eab55b5bd47017f6955498359785176bb5ae
jingeon <ephigh1397@naver.com>
2025-02-14 17:24:58 +0900

    feature: WebLink Entity 생성

---
+++ src/main/java/jg/aurora_world/entity/WebLink.java
@@ -0,0 +1,28 @@
+package jg.aurora_world.entity;
+
+import jakarta.persistence.*;
+import jg.aurora_world.enums.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class WebLink {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+}
