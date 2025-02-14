6b62faf7d4b02d92d68e797a8b3e65417d012d84
jingeon <ephigh1397@naver.com>
2025-02-14 17:04:31 +0900

    chore: init commit

---
+++ src/main/java/jg/aurora_world/AuroraWorldApplication.java
@@ -0,0 +1,13 @@
+package jg.aurora_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuroraWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jg/aurora_world/AuroraWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package jg.aurora_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
