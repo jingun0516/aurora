9c274cb04c282bb66b449217c077e0b955c60ad0
jingeon <ephigh1397@naver.com>
2025-02-16 23:50:09 +0900

    feature: 메인 페이지에 유저 목록 추가

--- src/main/java/jg/aurora_world/controller/view/MainViewController.java
+++ src/main/java/jg/aurora_world/controller/view/MainViewController.java
@@ -1,10 +1,19 @@
 package jg.aurora_world.controller.view;
 
+import jg.aurora_world.entity.Users;
+import jg.aurora_world.service.UsersService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainViewController {
+    private final UsersService usersService;
+
     @GetMapping("/")
     public String main() {
         return "index";
@@ -18,5 +27,14 @@ public class MainViewController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        List<Users> users = usersService.getUsers();
+
+        model.addAttribute("users", users);
+
+        return "users";
     }
 }
