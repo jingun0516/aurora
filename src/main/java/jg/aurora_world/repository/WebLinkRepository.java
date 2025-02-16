f1e34f2525ca9c141e63e97954a96dcb60ba9b58
jingeon <ephigh1397@naver.com>
2025-02-16 23:53:27 +0900

    feature: WebLinkRepository 작성자 이름으로 조회하는 메서드 추가

--- src/main/java/jg/aurora_world/repository/WebLinkRepository.java
+++ src/main/java/jg/aurora_world/repository/WebLinkRepository.java
@@ -4,6 +4,9 @@ import jg.aurora_world.entity.WebLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WebLinkRepository extends JpaRepository<WebLink, Long> {
+    List<WebLink> findAllByCreatedBy(String createdBy);
 }
