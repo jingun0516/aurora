7e8fcff7b172c3ae47638ba1aa670aeb0fc2a0a5
jingeon <ephigh1397@naver.com>
2025-02-18 16:58:58 +0900

    feature: 읽기 권한이 있는 웹링크에 대해서만 수정 버튼을 나타내도록 변경

--- src/main/java/jg/aurora_world/controller/view/WebLinkViewController.java
+++ src/main/java/jg/aurora_world/controller/view/WebLinkViewController.java
@@ -1,21 +1,29 @@
 package jg.aurora_world.controller.view;
 
+import jg.aurora_world.entity.Users;
 import jg.aurora_world.entity.WebLink;
 import jg.aurora_world.enums.PermissionType;
+import jg.aurora_world.service.UsersService;
+import jg.aurora_world.service.WebLinkPermissionService;
 import jg.aurora_world.service.WebLinkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/weblink")
 @RequiredArgsConstructor
 public class WebLinkViewController {
     private final WebLinkService webLinkService;
+    private final UsersService usersService;
+    private final WebLinkPermissionService webLinkPermissionService;
 
     @GetMapping("/register")
     public String webLinkRegister() {
@@ -24,11 +32,20 @@ public class WebLinkViewController {
 
     @GetMapping("/all")
     public String readableWebLink(Model model) {
-        List<WebLink> readableWebLinks = webLinkService.getAllWebLinkByPermissionType(PermissionType.READ);
-        model.addAttribute("readableWebLinks", readableWebLinks);
-
+        Long userId = usersService.getLoggedInUser().getId();
         List<WebLink> myWebLinks = webLinkService.getAllMyWebLinks();
+        Map<Long, Boolean> canWriteMap = new HashMap<>();
+
+        List<WebLink> readableWebLinks = webLinkService.getAllWebLinkByPermissionType(PermissionType.READ);
+
+        for (WebLink webLink : readableWebLinks) {
+            boolean canWrite = webLinkPermissionService.canWriteWebLink(userId, webLink.getId());
+            canWriteMap.put(webLink.getId(), canWrite);
+        }
+
         model.addAttribute("myWebLinks", myWebLinks);
+        model.addAttribute("canWriteMap", canWriteMap);
+        model.addAttribute("readableWebLinks", readableWebLinks);
 
         return "weblinks";
     }
