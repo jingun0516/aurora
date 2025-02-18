1f5df632b0199d03c1c8d6df4f7441e3380dc860
jingeon <ephigh1397@naver.com>
2025-02-18 16:59:57 +0900

    feature: 읽기권한 여부 반환 / 권한 삭제 메서드 추가

--- src/main/java/jg/aurora_world/service/WebLinkPermissionService.java
+++ src/main/java/jg/aurora_world/service/WebLinkPermissionService.java
@@ -1,5 +1,6 @@
 package jg.aurora_world.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import jg.aurora_world.entity.Users;
 import jg.aurora_world.entity.WebLink;
 import jg.aurora_world.entity.WebLinkPermission;
@@ -8,6 +9,7 @@ import jg.aurora_world.repository.WebLinkPermissionRepository;
 import jg.aurora_world.repository.WebLinkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,20 +21,28 @@ public class WebLinkPermissionService {
     private final UsersService usersService;
     private final WebLinkRepository webLinkRepository;
 
+    @Transactional
     public WebLinkPermission addWebLinkPermission(Long userId, Long webLinkId, PermissionType permissionType) {
-        WebLinkPermission webLinkPermission = new WebLinkPermission();
         Users user = usersService.getById(userId);
         WebLink webLink = webLinkRepository.findById(webLinkId).orElse(null);
 
-        if(user == null || webLink == null) {
-            return null;
+        if (user == null || webLink == null) {
+            throw new EntityNotFoundException("User or WebLink not found");
         }
 
+        boolean exists = webLinkPermissionRepository.existsByUsersAndWebLinkAndPermissionType(user, webLink, permissionType);
+        if (exists) {
+            throw new IllegalStateException("Permission already exists.");
+        }
+
+        WebLinkPermission webLinkPermission = new WebLinkPermission();
         webLinkPermission.setUsers(user);
         webLinkPermission.setWebLink(webLink);
         webLinkPermission.setPermissionType(permissionType);
+
         return webLinkPermissionRepository.save(webLinkPermission);
     }
+
 
     public List<WebLinkPermission> getAllWebLinkByPermissionType(Long userId, PermissionType permissionType) {
         Users user = usersService.getById(userId);
@@ -48,7 +58,27 @@ public class WebLinkPermissionService {
         return permissionsByType;
     }
 
-    public void deleteWebLinkPermission(Long weblinkPermissionId) {
-        webLinkPermissionRepository.deleteById(weblinkPermissionId);
+    public Boolean canWriteWebLink(Long userId, Long webLinkId) {
+        Users user = usersService.getById(userId);
+        WebLink webLink = webLinkRepository.findById(webLinkId).orElse(null);
+        if(user == null || webLink == null) {
+            return false;
+        }
+        return webLinkPermissionRepository.existsByUsersAndWebLinkAndPermissionType(user, webLink, PermissionType.WRITE);
+    }
+
+    @Transactional
+    public boolean deleteWebLinkPermission(Long userId, Long webLinkId, PermissionType permissionType) {
+        Users user = usersService.getById(userId);
+        WebLink webLink = webLinkRepository.findById(webLinkId).orElse(null);
+        if(user == null || webLink == null) {
+            throw new EntityNotFoundException("user or webLink not found");
+        }
+        if(!webLinkPermissionRepository.existsByUsersAndWebLinkAndPermissionType(user, webLink, permissionType)) {
+            return false;
+        }
+
+        webLinkPermissionRepository.deleteByUsersAndWebLinkAndPermissionType(user, webLink, permissionType);
+        return true;
     }
 }
