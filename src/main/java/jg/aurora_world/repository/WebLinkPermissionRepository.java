41a7d2e43af217003d4bc63381fe2f8f6bde44ba
jingeon <ephigh1397@naver.com>
2025-02-18 16:59:36 +0900

    feature: 레포지토리에 존재 여부 / 삭제 메서드 추가

--- src/main/java/jg/aurora_world/repository/WebLinkPermissionRepository.java
+++ src/main/java/jg/aurora_world/repository/WebLinkPermissionRepository.java
@@ -1,7 +1,9 @@
 package jg.aurora_world.repository;
 
 import jg.aurora_world.entity.Users;
+import jg.aurora_world.entity.WebLink;
 import jg.aurora_world.entity.WebLinkPermission;
+import jg.aurora_world.enums.PermissionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.List;
 @Repository
 public interface WebLinkPermissionRepository extends JpaRepository<WebLinkPermission, Long> {
     List<WebLinkPermission> findAllByUsers(Users user);
+    Boolean existsByUsersAndWebLinkAndPermissionType(Users user, WebLink webLink, PermissionType permissionType);
+    void deleteByUsersAndWebLinkAndPermissionType(Users user, WebLink webLink, PermissionType permissionType);
 }
